eafd71bc8833b96907f9c8b1e217829bf623c6c4
Aayush <aayush.dl8901@gmail.com>
2025-09-29 16:51:37 +0530

    add analytics route, analytics controller

---
+++ server/controllers/analytics.controller.js
@@ -0,0 +1,78 @@
+const asyncHandler = require("../middleware/asyncHandler");
+const Product = require("../models/product.model");
+const User = require("../models/user.model");
+
+exports.getAnalytics = asyncHandler(async (req, res) => {
+  const totalUsers = await User.countDocuments();
+  const totalProducts = await Product.countDocuments();
+
+  const salesData = await Order.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalSales: { $sum: 1 },
+        totalRevenue: { $sum: "$totalAmount" },
+      },
+    },
+  ]);
+
+  // chart
+
+  const startDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 100);
+  const endDate = new Date();
+
+  const salesDataChart = await Order.aggregate([
+    {
+      $match: {
+        $createdAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+      },
+    },
+    {
+      $group: {
+        id: { $dateToString: { $format: "%y-%m-%d", date: "$createdAt" } },
+        sales: { $sum: 1 },
+        revenue: { $sume: "$totalAmount" },
+      },
+    },
+    {
+      $sort: { _id: 1 },
+    },
+  ]);
+
+  const datesArray = getDatesInRange(startDate, endDate);
+
+  const chartData = datesArray.map((date) => {
+    const found = salesDataChart.find((item) => item._id === date);
+
+    return {
+      date,
+      sales: found?.sales || 0,
+      revenue: found?.revenue || 0,
+    };
+  });
+
+  res.json({
+    success: true,
+    analytics: {
+      totalUsers,
+      totalProducts,
+      totalSales: salesData[0].totalSales || 0,
+      totalRevenue: salesData[0].totalRevenue || 0,
+      chartData,
+    },
+  });
+});
+
+const getDatesInRange = (startDate, endDate) => {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    dates.push(currentDate.toISOString().split("T")[0]);
+    currentDate.setDate(currentDate.setDate() + 1);
+  }
+  return dates;
+};
