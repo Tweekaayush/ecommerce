87e3c81a426935258e6783c7d3feafc7c94409b3
Aayush <aayush.dl8901@gmail.com>
2025-10-03 14:29:10 +0530

    update FailedPage, update SuccessPage

--- client/src/pages/SuccessPage.jsx
+++ client/src/pages/SuccessPage.jsx
@@ -18,31 +18,20 @@ const SuccessPage = () => {
   return (
     <section className="h-screen">
       <div className="container flex flex-col items-center justify-center h-full">
-        <div className="flex flex-col items-center shadow-card p-4">
-          <CircleCheckBig className="text-green-600 w-15 h-15" />
-          <h1 className="text-green-600 text-5xl tracking-wider font-extrabold mb-2">
-            Thank You!
-          </h1>
-          <p className="text-xl tracking-wider text-gray-700 mb-4">
-            Your order has been placed successfully.
-          </p>
-          <div className="p-4 bg-gray-200 w-full mb-4 rounded-sm">
-            <h1 className="heading-5">Order Id:</h1>
-            <p className="body-text">{orderId}</p>
-          </div>
-          <button
-            className="button-1 w-full mb-4"
-            onClick={() => navigate("/profile")}
-          >
-            go to your order
-          </button>
-          <button
-            className="button-1 w-full"
-            onClick={() => navigate("/browse")}
-          >
-            Continue Shopping
-          </button>
+        <CircleCheckBig className="text-green-600 w-15 h-15" />
+        <h1 className="text-green-600 text-5xl tracking-wider font-extrabold mb-2">
+          Thank You!
+        </h1>
+        <p className="text-xl tracking-wider text-gray-700 mb-4">
+          Your order has been placed successfully.
+        </p>
+        <div className="p-4 bg-gray-200 mb-4 rounded-sm">
+          <h1 className="heading-5">Order Id:</h1>
+          <p className="body-text">{orderId}</p>
         </div>
+        <button className="button-1 mb-4" onClick={() => navigate("/profile")}>
+          go to your order
+        </button>
       </div>
     </section>
   );
