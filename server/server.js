b5ab849c5586ae8a01cc63586efd63188fb0ace6
Aayush <aayush.dl8901@gmail.com>
2025-10-17 10:35:19 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -55,8 +55,12 @@ const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/client/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server Running");
   });
 }
 
