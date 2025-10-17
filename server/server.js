97f37665e95c354c2645c011d56145c1f64094db
Aayush <aayush.dl8901@gmail.com>
2025-10-17 10:07:59 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(fileUpload());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
