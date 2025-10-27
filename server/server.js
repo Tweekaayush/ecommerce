56c1dfec4c07dc61e2c6cdc4c087af3fafe2f856
Aayush <aayush.dl8901@gmail.com>
2025-10-27 15:46:27 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -51,19 +51,6 @@ app.use("/api/v1/analytics", analyticsRoute);
 app.use("/api/v1/wishlist", wishlistRoute);
 app.use("/api/v1/review", reviewRoute);
 
-// const __dirname1 = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/client/dist")));
-//   app.get(/.*/, (req, res) => {
-//     res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("Server Running");
-//   });
-// }
-
 app.get('/', (req, res)=>{
   res.send('Server is Up and Running')
 })
