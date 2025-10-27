c32aa4d579e35bdb8058c57c65b08c2ca347001f
Aayush <aayush.dl8901@gmail.com>
2025-10-27 14:47:26 +0530

    update client env and constants

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,6 +1,3 @@
-const BASE_URL =
-  import.meta.env.VITE_NODE_ENV === "development"
-    ? "http://localhost:5000/api/v1"
-    : "/api/v1";
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 export default BASE_URL;
