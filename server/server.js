6480295db45677fcc6a561bf2512913d536ab517
Aayush <aayush.dl8901@gmail.com>
2025-11-05 00:37:07 +0530

    initial setup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express');
+// const connectDB = require('./config/db')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const app = express();
+
+// connectDB()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended: true}))
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    })
+)
+
+app.listen(process.env.PORT, (req, res) => {
+    console.log('Server running on port:', process.env.PORT);
+})
